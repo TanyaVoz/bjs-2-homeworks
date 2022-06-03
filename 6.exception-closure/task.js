a66641ee502b739ac458e697cf0973dcf7e0436b
Tanya <voznyuk1998@mail.ru>
2022-06-03 22:17:59 +0300

    finish1,2

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -18,3 +18,40 @@ function validateCount(value) {
 }
 
 //з2
+class Triangle {
+    constructor(a, b, c) {// 3 стороны
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+        if (a + b < c || b + c < a || a + c < b) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+    }
+
+    getPerimeter() {  // периметр треугольника.
+        return this.a + this.b + this.c;
+    }
+
+    getArea() {
+        let half = 0.5 * this.getPerimeter();
+        let S = Math.sqrt(half * (half - this.a) * (half - this.b) * (half - this.c));// формулa Герона.
+
+        return + S.toFixed(3);
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);//объект треугольника
+    } catch (err) {//перехвата исключения
+        return {
+            getArea() {
+                return "Ошибка! Треугольник не существует";
+            },
+            getPerimeter() {
+                return "Ошибка! Треугольник не существует";
+            }
+        }
+    }
+}
