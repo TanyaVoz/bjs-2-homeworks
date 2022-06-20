025b86326c5e657048bb1d4724806940310bf197
Tanya <voznyuk1998@mail.ru>
2022-06-20 16:01:05 +0300

    правки

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,23 +1,25 @@
 //1
 function cachingDecoratorNew(func) {
-  let cache = [];
+  let cache = [
+    { hash: "10,20,30", value: 60 },
+    { hash: "2,2,2", value: 6 },
+  ];
 
   function wrapper(...args) {
 
     const hash = args.join(',');// получаем правильный хэш
-    let objectInCache = cache.findIndex((item) => item.hash === hash);//// ищем элемент, хэш которого равен нашему хэшу
-
-    if (objectInCache !== -1) {
-      console.log("Из кэша: " + cache[objectInCache].value);
-      return "Из кэша: " + cache[objectInCache].value;
+    let objectInCache = cache.find(item => item.hash === hash);//// ищем элемент, хэш которого равен нашему хэшу
+    //console.log(objectInCache);
+    if (objectInCache) {
+      console.log("Из кэша: " + objectInCache.value);
+      return "Из кэша: " + objectInCache.value;
+    }
+    if (cache.length > 5) {
+      cache.shift();//// если слишком много элементов в кэше надо удалить самый старый (первый) 
     }
 
     let result = func(...args);
     cache.push({ hash: hash, value: result });// // добавляем элемент
-
-    if (cache.length > 5) {
-      cache.shift();//// если слишком много элементов в кэше надо удалить самый старый (первый) 
-    }
 
     console.log("Вычисляем: " + result);
     return "Вычисляем: " + result;
@@ -28,7 +30,7 @@ function cachingDecoratorNew(func) {
 //2
 
 function debounceDecoratorNew(func, ms) {
-  let Debounced = false;
+  let Debounce = false;
   let timeout;
 
   return function (...args) {
@@ -38,12 +40,22 @@ function debounceDecoratorNew(func, ms) {
       func(...args);
     }, ms);
 
-    if (!Debounced) {
+    if (!Debounce) {
       func(...args);
-      Debounced = true;
+      Debounce = true;
     }
   }
 }
 
+//3
 
+function debounceDecorator2(debounceDecoratorNew) {
+  wrapper.count = 0;
+
+  function wrapper(...rest) {
+    wrapper.count++;
+    return debounceDecoratorNew.call(this, ...rest);
+  }
+  return wrapper;
+}
 
